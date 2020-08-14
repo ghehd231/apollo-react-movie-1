508801a65bbcab037628b5bd8876d87c0ae5933d
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-08-14 17:34:17 +0900

    apollo client 세팅 && API 연동 테스트 (graphql-movie-backend)

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,19 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
 
 const Home = () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
   return <div>Home</div>;
 };
 
