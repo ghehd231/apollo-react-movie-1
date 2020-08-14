b3238391266f5daea969ef696af9d8bc63cb957f
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-08-14 17:53:19 +0900

    Home 화면 약간 퍼블리싱 && movie API 연동 (영화 상세 화면)

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Apollo React Movie
 
 Movie app built with React, Apollo and GraphQL
+
+API: graphql-movie-backend
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      rating
+      language
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
 
 const Detail = () => {
-  return <div>Detail</div>;
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_MOVIE, {
+    variables: { id: Number(id) },
+  });
+  if (loading) return "loading...";
+  else if (!loading && !error && data && data.movie) {
+    return data.movie.title;
+  } else {
+    return "error...";
+  }
 };
 
 export default Detail;
